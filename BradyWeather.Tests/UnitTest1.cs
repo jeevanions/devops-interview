b0fff3496559f6f93bb3f90c36a85ac6568d4248
jeevanions <95138087+jeevanions@users.noreply.github.com>
2021-11-29 03:01:04 +0000

    Added empty unit test project

---
+++ BradyWeather.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace BradyWeather.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //TODO: Emty Test to run as part of the pipeline
+
+        }
+    }
+}
